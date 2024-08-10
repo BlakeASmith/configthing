1a70ad58f03bc6490d4eb61c8b84e55dae7eefc8
= <blake.woodsmith@gmail.com>
2024-08-09 21:17:35 -0700

    feat: more scaffolding

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install script
 # This script should not assume any dependencies are present
@@ -19,5 +19,31 @@ fi
 
 # setup brew, since it works on both Mac and Linux
 # https://brew.sh/ says that this will work on both
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	# Brew needs you to do some stuff after installing
+	echo "Please follow the above instructions from brew and re-run the configthing install!"
+	exit 0
+else
+	echo "found brew!"
+fi
+
+# setup tmux
+if ! command -v tmux &> /dev/null; then
+	brew install tmux
+else
+	echo "found tmux!"
+fi
+
+
+# At this point, the install needs the configthing files
+# If this install script is running in isolation, it will need to clone the repo
+# If it is already in the repo, there is no need to clone
+if [ -d .configthing.thisisdefinitelyconfigthing.txt ]; then
+	echo "configthing"
+else
+	echo "notconfigthing"
+fi
+
 
